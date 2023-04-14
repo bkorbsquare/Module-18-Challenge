d51f3a59b660a8e4295564ec47a41cd9dd0c00f5
Brandon <110788407+bkorbsquare@users.noreply.github.com>
2023-04-14 00:22:38 -0500

    models

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+// Connect to the Mongo DB
 mongoose.connect('mongodb+srv://bkorbsquare:Steamship1@cluster0.bxqmt3n.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
--- models/user.js
+++ models/user.js
@@ -1,4 +1,4 @@
-const { Schema, model, Types } = require('mongoose');
+const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema(
   {
@@ -12,10 +12,7 @@ const userSchema = new Schema(
       type: String,
       required: true,
       unique: true,
-      match: [
-        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-        "Please use a valid email address",
-      ],
+      match: [/^([a-z0-9_.-]+)@([\da-z.-]+).([a-z.]{2,6})$/],
     },
     thoughts: [
       {
@@ -41,5 +38,7 @@ const userSchema = new Schema(
 userSchema.virtual("friendCount").get(function () {
   return this.friends.length;
 });
+
 const User = model('User', userSchema);
+
 module.exports = User;
